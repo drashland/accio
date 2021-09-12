f54576ecf5337cac059c6a0a842d269ee0b3206d
Eric Crooks <eric.crooks@gmail.com>
2021-09-11 20:37:43 -0400

    fix: issue with deno copied files permissions

--- console/build_deno_lib.ts
+++ console/build_deno_lib.ts
@@ -6,16 +6,19 @@ Deno.run({ cmd: ["cp", "src/accio.ts", "lib/deno/accio.ts"] });
 Deno.run({ cmd: ["cp", "src/collection.ts", "lib/deno/collection.ts"] });
 Deno.run({ cmd: ["cp", "src/document.ts", "lib/deno/document.ts"] });
 Deno.run({ cmd: ["cp", "src/field_type.ts", "lib/deno/field_type.ts"] });
+Deno.run({ cmd: ["chmod", "700", "lib/deno/*.ts"] });
 
 const filesToRewrite = [
-  "./lib/deno/accio.ts",
-  "./lib/deno/collection.ts",
-  "./lib/deno/document.ts",
-  "./lib/deno/field_type.ts",
+  "/lib/deno/accio.ts",
+  "/lib/deno/collection.ts",
+  "/lib/deno/document.ts",
+  "/lib/deno/field_type.ts",
 ]
 
 filesToRewrite.forEach((file: string) => {
-  let contents = decoder.decode(Deno.readFileSync(file));
+  let filepath = Deno.realPathSync(".") + file;
+  console.log(`Writing the following file:`, filepath);
+  let contents = decoder.decode(Deno.readFileSync(filepath));
 
   const importStatements = contents.match(/import.*";/g);
 
